acae0159a7d882d511fed38ebb9c1a99de56cb82
noah-harding <m252328@usna.edu>
2022-11-04 09:47:23 -0400

    blited some grass

--- mighty_midy.py
+++ mighty_midy.py
@@ -1,1 +1,26 @@
-print("hello")
+import pygame
+import sys
+
+pygame.init()
+
+TILE_SIZE = 64
+
+screen = pygame.display.set_mode((10*TILE_SIZE, 10*TILE_SIZE))
+grass = pygame.image.load("images/Default/roadTexture_25.png")
+grass_rect = grass.get_rect()
+screen_rect = screen.get_rect()
+
+num_tiles = screen_rect.width // grass_rect.width
+for y in range(num_tiles):
+    for x in range(num_tiles):
+        screen.blit(grass, (x*grass_rect.width, y*grass_rect.height))
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    pygame.display.flip()
+
