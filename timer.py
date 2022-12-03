eb44a5253d7f0f4a8bc7232f82a6c5017663f83c
noah-harding <m252328@usna.edu>
2022-12-03 16:28:22 -0500

    flappy fish init

---
+++ timer.py
@@ -0,0 +1,26 @@
+import pygame
+import settings
+from pygame.sprite import Sprite
+
+class Timer(Sprite):
+    def __init__(self):
+        super().__init__()
+        self.font = pygame.font.Font(None, 80)
+
+    def update(self, screen):
+        total_seconds = settings.FRAME_COUNT // settings.FRAME_RATE
+        minutes = total_seconds // 60
+        seconds = total_seconds % 60
+        time_display = "{0:02}:{1:02}".format(minutes, seconds)
+        text = self.font.render(time_display, True, (227, 124, 7))
+        screen.blit(text, (20, 20))
+        total_seconds = settings.START_TIME + (settings.FRAME_COUNT // settings.FRAME_RATE)
+        settings.FRAME_COUNT += 1
+        if total_seconds > 10:
+            settings.OBSTICALE_SPEED = 8
+        if total_seconds > 20:
+            settings.OBSTICALE_SPEED = 11
+        if total_seconds > 30:
+            settings.OBSTICALE_SPEED = 14
+        if total_seconds > 40:
+            settings.OBSTICALE_SPEED = 17
